fd34df4f6950d2e283b2c99930f255b54a54cd02
Gabriel Delight <gabrieldelight08@gmail.com>
2024-03-03 17:44:43 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import {Text} from 'react-native';
 import React from 'react';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 
 import Home from './Screens/Home';
 import Settings from './Screens/Settings';
@@ -11,53 +12,32 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Foundation from 'react-native-vector-icons/Foundation';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Chat from './Screens/Chat';
-import {NavigationContainer} from '@react-navigation/native';
 
-const RootStack = createMaterialBottomTabNavigator();
+const RootStack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <RootStack.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, size, color}) => {
-            let IconName;
-
-            color = focused ? 'hotpink' : 'white';
-
-            if (route.name == 'Home') {
-              IconName = 'greenhouse';
-              size = focused ? 25 : 20;
-              return (
-                <MaterialCommunityIcons
-                  name={IconName}
-                  color={color}
-                  size={size}
-                />
-              );
-            } else if (route.name == 'Chat') {
-              IconName = 'chat';
-              size = focused ? 25 : 20;
-              return (
-                <MaterialCommunityIcons
-                  name={IconName}
-                  color={color}
-                  size={size}
-                />
-              );
-            } else if (route.name == 'Settings') {
-              IconName = 'gear';
-              size = focused ? 25 : 20;
-              return <FontAwesome name={IconName} color={color} size={size} />;
-            }
+        initialRouteName="BottomTabs"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: 'black',
           },
-        })}
-        // for material ui styles
-        activeColor="white"
-        inactiveColor="#3e2465"
-        barStyle={{backgroundColor: 'hotpink'}}>
-        <RootStack.Screen name="Home" component={Home} />
-        <RootStack.Screen name="Chat" component={Chat} />
-        <RootStack.Screen name="Settings" component={Settings} />
+          headerTintColor: '#bbb',
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: 'normal',
+          },
+        }}>
+        <RootStack.Screen
+          name="BottomTabs"
+          options={{
+            header: () => null,
+            title: 'Home',
+          }}
+          component={BottomTabs}
+        />
       </RootStack.Navigator>
     </NavigationContainer>
   );
